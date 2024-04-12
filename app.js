a2218241d796444afb37b55b51311ea300cfeb30
Giovana Assis <giovanaassisstos@gmail.com>
2024-04-12 15:44:19 -0300

    Excluindo o log

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else{
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados)
         return numeroEscolhido;
     }
 }
